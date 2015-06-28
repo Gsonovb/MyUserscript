9269bf93220f6bcf766c22ca589b20cba560741a
Guanyc <guanyc@gmail.com>
2015-06-28 16:59:35 +0800

    更新脚本
    适用于新版界面

--- MSDN Magazine Clean.js
+++ MSDN Magazine Clean.js
@@ -1,24 +1,28 @@
 ﻿// ==UserScript==
 // @name         MSDN magazine Clean
 // @namespace    http://your.homepage/
-// @version      0.1
+// @version      0.2
 // @description  清理MSDN杂志页面的广告
 // @author       Guanyc
 // @match        http*://msdn.microsoft.com/*/magazine/*
 // @grant        none
 // ==/UserScript==
 
-var container = document.getElementsByClassName("EyebrowContainer")[0];
+var content = document.getElementById("content");
+var container = document.createElement("div");
+
+content.insertBefore(container,content.childNodes[0]);
 
 var span=document.createElement("span");
-span.textContent="  |  "
+span.textContent=" Helper : ";
 container.appendChild(span);
 
 
-var btnhidecode=document.createElement("a");
+//btnhidecode.href="#";
+var btnhidecode=document.createElement("button");
 btnhidecode.textContent="Remove Code Part";
 btnhidecode.onclick=function(){
-    var items = document.getElementsByClassName("CodeHighlighter");
+    var items = document.getElementsByClassName("codeSnippetContainer");
 
     for(i=0;i<items.length;i++){
         items[i].hidden=true;
@@ -28,16 +32,19 @@ btnhidecode.onclick=function(){
 container.appendChild(btnhidecode);
 
 
+
 var span=document.createElement("span");
 span.textContent="  |  "
 container.appendChild(span);
 
 
-var btnhideright=document.createElement("a");
+var btnhideright=document.createElement("button");
 btnhideright.textContent=" Remove Right Content";
 btnhideright.onclick=function(){
     var item=document.getElementById("RightContent");
-    item.parentElement.remove()
+    item.remove()
+	var lc=document.getElementById("LeftContent");
+	lc.style.width="100%";
 }
 
 container.appendChild(btnhideright);
@@ -48,17 +55,12 @@ span.textContent="  |  "
 container.appendChild(span);
 
 
-var btncolor=document.createElement("a");
+var btncolor=document.createElement("button");
 btncolor.textContent="Change back color";
 btncolor.onclick=function(){
-    var Content = document.getElementById("MainContent");
-    Content.style.backgroundColor="#aaa";
+    var Content = document.getElementById("page");
+    Content.style.backgroundColor="#ddd";
     
-    var Content2 = document.getElementById("BodyBackground");
-    Content2.style.backgroundColor="#aaa";
-    
-    
-    document.getElementsByClassName("MsdnPageBody")[0].style.backgroundColor="#bbb";
     
 }
 
